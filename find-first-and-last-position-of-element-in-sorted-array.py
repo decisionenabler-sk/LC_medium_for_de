828501541c3a478c700102b04310b605ae2a6f61
decisionenabler-sk <surbhi.katy@gmail.com>
2023-12-29 13:01:35 -0800

    first and last element in sorted array (brute force)

---
+++ find-first-and-last-position-of-element-in-sorted-array.py
@@ -0,0 +1,19 @@
+def searchRange(nums, target):
+        first = -1
+        last = -1
+        for i in range(len(nums)):
+            if nums[i] == target:
+                if first == -1:
+                    first = i
+                last = i
+        return first, last
+
+# test 1
+nums1 = [5,7,7,8,8,10]
+target1 = 8
+print(searchRange(nums1, target1))
+
+# test 2
+nums2 = [5,7,7,8,8,10]
+target2 = 6
+print(searchRange(nums2, target2))
